9b15e9a996311f7eee4eddcc424886f63bb71054
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-04-12 11:29:46 +0900

    stwc

--- stop watch/ViewController.swift
+++ stop watch/ViewController.swift
@@ -1,25 +1,59 @@
-//
-//  ViewController.swift
-//  stop watch
-//
-//  Created by D7702_10 on 2018. 4. 12..
-//  Copyright © 2018년 jik. All rights reserved.
-//
 
-import UIKit
+    import UIKit
+        
+        class ViewController: UIViewController {
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+        
+            @IBOutlet weak var rt: UILabel!
+            @IBOutlet weak var sta: UIButton!
+            @IBOutlet weak var stoo: UIButton!
+            @IBOutlet weak var res: UIButton!
+            var count = 0
+            var myTimer = Timer()
+            override func viewDidLoad() {
+                super.viewDidLoad()
+                
+                
+            }
+          
+            @IBAction func st(_ sender: Any) {
+            
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in
+                    self.updateTime()
+                });
+                changeButtonState(start: false, stop: true, reset: true)
+            }
+         
+            @IBAction func sto(_ sender: Any) {
+            myTimer.invalidate()
+                changeButtonState(start: true, stop: false, reset: true)
+            }
+            
+            
+            
+            @IBAction func re(_ sender: Any) {
+            myTimer.invalidate()
+                count = 0
+                rt.text = String(format:"00:00:00")
+                changeButtonState(start: true, stop: true, reset: false)
+            }
+            func changeButtonState(start: Bool, stop: Bool, reset: Bool){
+                sta.isEnabled = start
+                stoo.isEnabled = stop
+                res.isEnabled = reset
+            }
+            func updateTime() {
+                count = count + 1
+                let min = count / 60 / 100
+                let sec = (count - (min * 60 * 100)) / 100
+                let msec = count - (min * 60 * 100) - (sec * 100)
+                rt.text = String(format: "%02d:%02d:%02d", min, sec, msec)
+            }
+            
+            override func didReceiveMemoryWarning() {
+                super.didReceiveMemoryWarning()
+                // Dispose of any resources that can be recreated.
+            }
 
 }
 
